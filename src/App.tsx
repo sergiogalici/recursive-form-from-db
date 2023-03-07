c3ca5aa8540a9ba7299ae7b1609edeaf85bb0d83
Sergio Galici <galici.sergio@gmail.com>
2023-03-07 09:53:32 +0100

    New Form Created

--- src/App.tsx
+++ src/App.tsx
@@ -1,68 +1,103 @@
 import React from "react";
-import { useForm, SubmitHandler } from "react-hook-form";
+import { useForm, useFieldArray, useWatch, Control } from "react-hook-form";
 
-interface IFormInputs {
-  firstName: string;
-  lastName: string;
-  numberOfRecidencies: number;
-  addresses: AddressType[];
-}
-
-type AddressType = {
-  address: string;
-  city: string;
+type FormValues = {
+  cart: {
+    name: string;
+    price: number;
+    quantity: number;
+  }[];
 };
 
-const onSubmit: SubmitHandler<IFormInputs> = (data) => console.log(data);
+const Total = ({ control }: { control: Control<FormValues> }) => {
+  const formValues = useWatch({
+    name: "cart",
+    control,
+  });
+  const total = formValues.reduce(
+    (acc, current) => acc + (current.price || 0) * (current.quantity || 0),
+    0
+  );
+  return <p>Total Amount: {total}</p>;
+};
 
 const App = () => {
   const {
     register,
-    formState: { errors },
+    control,
     handleSubmit,
-    watch,
-  } = useForm<IFormInputs>({
+    formState: { errors },
+  } = useForm<FormValues>({
     defaultValues: {
-      firstName: "",
-      lastName: "",
-      numberOfRecidencies: 1,
+      cart: [{ name: "test", quantity: 1, price: 23 }],
     },
+    mode: "onBlur",
   });
 
-  const selectedResidencies = Number(watch("numberOfRecidencies"));
+  const { fields, append, remove } = useFieldArray({
+    name: "cart",
+    control,
+  });
 
-  const arrOfRes = Array.from(Array(selectedResidencies));
+  const onSubmit = (data: FormValues) => console.log(data);
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <input {...register("firstName", { required: true })} />
-      {errors.firstName && "First name is required"}
-      <input {...register("lastName", { required: true })} />
-      {errors.lastName && "Last name is required"}
-      <input
-        min={1}
-        type="number"
-        {...register("numberOfRecidencies", { min: 1 })}
-      />
-      {errors.numberOfRecidencies && "Not a valid input"}
-      {arrOfRes.map((_, i) => {
-        return (
-          <div key={Date.now() * Math.random()} className="input-container">
-            <p>Address #{i + 1}</p>
-            <input
-              placeholder={`Insert address`}
-              {...register(`addresses.${i}.address`)}
-            />
-            <input
-              placeholder={`Insert city`}
-              {...register(`addresses.${i}.city`)}
-            />
-          </div>
-        );
-      })}
+    <div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        {fields.map((field, index) => {
+          return (
+            <div key={field.id}>
+              <section className={"section"} key={field.id}>
+                <input
+                  placeholder="name"
+                  {...register(`cart.${index}.name` as const, {
+                    required: true,
+                  })}
+                  className={errors?.cart?.[index]?.name ? "error" : ""}
+                />
+                <input
+                  placeholder="quantity"
+                  type="number"
+                  {...register(`cart.${index}.quantity` as const, {
+                    valueAsNumber: true,
+                    required: true,
+                  })}
+                  className={errors?.cart?.[index]?.quantity ? "error" : ""}
+                />
+                <input
+                  placeholder="value"
+                  type="number"
+                  {...register(`cart.${index}.price` as const, {
+                    valueAsNumber: true,
+                    required: true,
+                  })}
+                  className={errors?.cart?.[index]?.price ? "error" : ""}
+                />
+                <button type="button" onClick={() => remove(index)}>
+                  DELETE
+                </button>
+              </section>
+            </div>
+          );
+        })}
 
-      <input type="submit" />
-    </form>
+        <Total control={control} />
+
+        <button
+          type="button"
+          onClick={() =>
+            append({
+              name: "",
+              quantity: 0,
+              price: 0,
+            })
+          }
+        >
+          APPEND
+        </button>
+        <input type="submit" />
+      </form>
+    </div>
   );
 };
 
